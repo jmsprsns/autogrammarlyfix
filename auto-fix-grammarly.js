b1ac69af588e45f3f3e0a827f3cc84ab47bdd2dd
jmsprsns <61780869+jmsprsns@users.noreply.github.com>
2023-11-30 15:37:27 -0800

    Create auto-fix-grammarly.js

---
+++ auto-fix-grammarly.js
@@ -0,0 +1,22 @@
+
+function refreshData() {
+x = 1;
+try {
+if (document.querySelector('.cards-replacements_labels-itemRemove') !== null) {
+var newclass = document.querySelector('.cards-replacements_labels-itemRemove');
+newclass.click();
+} else {
+var newclass = document.getElementsByClassName("cards-replacements_labels-itemInsert");
+newclass = newclass[0];
+newclass.click();
+}
+} catch (error) {
+if (document.getElementsByClassName("wrapper_fipkee2") !== null) {
+var cli = document.getElementsByClassName("wrapper_fipkee2");
+var vli = cli[0];
+vli.click();
+}
+}
+setTimeout(refreshData, x * 1000);
+}
+refreshData();
