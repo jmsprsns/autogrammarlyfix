10f45f6903f66177380f797690153808ac5c691b
jmsprsns <61780869+jmsprsns@users.noreply.github.com>
2023-11-30 16:39:57 -0800

    Update auto-fix-grammarly.js

--- auto-fix-grammarly.js
+++ auto-fix-grammarly.js
@@ -1,6 +1,7 @@
 var refreshTimer;
 var lastValue = null;
 var unchangedCount = 0;
+var observer;
 
 function refreshData() {
     x = 1; 
@@ -38,4 +39,40 @@ function refreshData() {
     refreshTimer = setTimeout(refreshData, x * 1000);
 }
 
+function checkForChanges() {
+    var currentValue = document.querySelector('.counterText').textContent;
+    if (currentValue === lastValue) {
+        unchangedCount++;
+        if (unchangedCount >= 5) {
+            console.log("Value unchanged for 5 refreshes, stopping script.");
+            clearTimeout(refreshTimer);
+            observer.disconnect();
+            return;
+        }
+    } else {
+        unchangedCount = 0;
+        lastValue = currentValue;
+    }
+}
+
+function startObserving() {
+    var targetNode = document.querySelector('.counterText');
+    if (!targetNode) {
+        console.log("Target node not found");
+        return;
+    }
+
+    var config = { characterData: true, childList: true, subtree: true };
+
+    observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            checkForChanges();
+        });
+    });
+
+    observer.observe(targetNode, config);
+}
+
+// Start the process
 refreshData();
+startObserving();
