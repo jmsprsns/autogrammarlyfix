7d710c7accf08eaf8b315dee6bc9e617501c90b9
jmsprsns <61780869+jmsprsns@users.noreply.github.com>
2023-11-30 16:26:42 -0800

    Update auto-fix-grammarly.js

--- auto-fix-grammarly.js
+++ auto-fix-grammarly.js
@@ -1,22 +1,24 @@
+var refreshTimer;
 
 function refreshData() {
-x = 1;
-try {
-if (document.querySelector('.cards-replacements_labels-itemRemove') !== null) {
-var newclass = document.querySelector('.cards-replacements_labels-itemRemove');
-newclass.click();
-} else {
-var newclass = document.getElementsByClassName("cards-replacements_labels-itemInsert");
-newclass = newclass[0];
-newclass.click();
+    x = 1;
+    try {
+        if (document.querySelector('.cards-replacements_labels-itemRemove') !== null) {
+            var newclass = document.querySelector('.cards-replacements_labels-itemRemove');
+            newclass.click();
+        } else {
+            var newclass = document.getElementsByClassName("cards-replacements_labels-itemInsert");
+            newclass = newclass[0];
+            newclass.click();
+        }
+    } catch (error) {
+        if (document.getElementsByClassName("wrapper_fipkee2") !== null) {
+            var cli = document.getElementsByClassName("wrapper_fipkee2");
+            var vli = cli[0];
+            vli.click();
+        }
+    }
+    refreshTimer = setTimeout(refreshData, x * 1000);
 }
-} catch (error) {
-if (document.getElementsByClassName("wrapper_fipkee2") !== null) {
-var cli = document.getElementsByClassName("wrapper_fipkee2");
-var vli = cli[0];
-vli.click();
-}
-}
-setTimeout(refreshData, x * 1000);
-}
+
 refreshData();
