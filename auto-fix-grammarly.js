1698cc40e2addb3459ca7b05b489eeb9efeac44f
jmsprsns <61780869+jmsprsns@users.noreply.github.com>
2024-01-24 10:25:05 -0800

    Update auto-fix-grammarly.js

--- auto-fix-grammarly.js
+++ auto-fix-grammarly.js
@@ -4,7 +4,7 @@ var unchangedCount = 0;
 var observer;
 
 function refreshData() {
-    console.log("Running refreshData function");
+    console.log("GrammarlyAutofix: Running refreshData function");
     try {
         var itemRemove = document.querySelector('.cards-replacements_labels-itemRemove, .cards-replacements_labels-deleteAll');
         var dismissButton = document.querySelector('button[data-name="card/ignore"], button[data-name="card/bulk-accept-apply"]');
@@ -14,24 +14,24 @@ function refreshData() {
         setTimeout(function() {
             
             if (itemRemove) {
-                console.log("Grammar mistake fixed!");
+                console.log("GrammarlyAutofix: Grammar mistake fixed!");
                 itemRemove.click();
                 actionable = true; // There is an action, so set flag to true
             } else {
-                console.log("Dismissed item without any solution.");
+                console.log("GrammarlyAutofix: Dismissed item without any solution.");
                 dismissButton.click();
                 actionable = true;
             }
     
             if (updateAllButton) {
-                console.log("Clicking updateAllButton");
+                console.log("GrammarlyAutofix: Clicking updateAllButton");
                 updateAllButton.click();
                 actionable = true;
             }
     
             if (!actionable) {
                 unchangedCount++;
-                console.log("Unchanged Count: ", unchangedCount);
+                console.log("GrammarlyAutofix: Unchanged Count: ", unchangedCount);
                 if (unchangedCount >= 10) {
                     clearTimeout(refreshTimer);
                     if (observer) {
@@ -47,7 +47,7 @@ function refreshData() {
         }, 250); // Timeout
         
     } catch (error) {
-        console.log("Error in refreshData: ", error);
+        console.log("GrammarlyAutofix: Error in refreshData: ", error);
     }
 
     refreshTimer = setTimeout(refreshData, 500);
@@ -55,23 +55,23 @@ function refreshData() {
 
 
 function checkForChanges() {
-    console.log("Running checkForChanges function");
+    console.log("GrammarlyAutofix: Running checkForChanges function");
     refreshData(); // Call refreshData to handle dynamic content
 }
 
 function startObserving() {
-    console.log("Starting to observe");
+    console.log("GrammarlyAutofix: Starting to observe");
     var targetNode = document.querySelector('.counterText'); // Adjust if a different parent element is more appropriate
 
     if (!targetNode) {
-        console.log("Target node not found");
+        console.log("GrammarlyAutofix: Target node not found");
         return;
     }
 
     var config = { childList: true, subtree: true };
 
     observer = new MutationObserver(function(mutations) {
-        console.log("Mutation observed");
+        console.log("GrammarlyAutofix: Mutation observed");
         checkForChanges();
     });
 
@@ -79,6 +79,6 @@ function startObserving() {
 }
 
 // Start the process
-console.log("Starting the process");
+console.log("GrammarlyAutofix: Starting the process");
 refreshData();
 startObserving();
